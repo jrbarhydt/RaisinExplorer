079d2485905e932abd0917ca11c66ec4680856bc
JR Barhydt <jrbarhydt@zapenergyinc.com>
2022-08-07 12:33:26 -0700

    cleanup, document, remove unused dependencies

--- Raisins.py
+++ Raisins.py
@@ -5,12 +5,22 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
-from sklearn.decomposition import PCA
 import numpy as np
+
 
 # ======================================================================================================================
 # Functions
 # ----------------------------------------------------------------------------------------------------------------------
+
+def gen_ellipse(center, width, height, angle, color, label):
+    ellipse = Ellipse(xy=center,
+                      width=width,
+                      height=height,
+                      angle=angle,
+                      linewidth=2, ec=color, linestyle='', label=label)
+    ellipse.set_alpha(0.2)
+    ellipse.set_facecolor(color)
+    return ellipse
 
 
 # ======================================================================================================================
@@ -50,48 +60,29 @@ with st.sidebar:
 if selected[0] == selected[1]:
     st.write("Must chose two different axes to compare.")
 else:
-    pca = PCA(n_components=2)
-    x, y = list(df[df["Class"] == "Kecimen"][selected[0]]), list(df[df["Class"] == "Kecimen"][selected[1]])
-    pca.fit(list(zip(x, y)))
-    cov = np.cov(x, y)
-    lambda_, v = np.linalg.eig(cov)
-    lambda_ = np.sqrt(lambda_)
-    ell = Ellipse(xy=(np.mean(x), np.mean(y)),
-                  width=lambda_[0] * 2, height=lambda_[1] * 2,
-                  angle=np.rad2deg(np.arccos(v[0, 0])),
-                  linewidth=2, ec='#3d8ac0', linestyle='', label="Kecimen")
-    ell.set_alpha(0.2)
-    ell.set_facecolor('#3d8ac0')
-    ell1 = Ellipse(xy=(np.mean(x), np.mean(y)),
-                   width=lambda_[0] * 4, height=lambda_[1] * 4,
-                   angle=np.rad2deg(np.arccos(v[0, 0])),
-                   linewidth=2, ec='#3d8ac0', linestyle='', label="Kecimen")
-    ell1.set_alpha(0.2)
-    ell1.set_facecolor('#3d8ac0')
+    # kecimen ellipses from eigenvectors/values
+    xy = df[df["Class"] == "Kecimen"][[selected[0], selected[1]]]
+    cov = np.cov(xy.transpose())
+    e_val, e_vec = np.linalg.eig(cov)
+    w, h = 2 * np.sqrt(e_val)
+    theta = np.rad2deg(np.arccos(e_vec[0, 0]))
+    ell = gen_ellipse(xy.mean(), w, h, theta, '#3d8ac0', "Kecimen")
+    ell1 = gen_ellipse(xy.mean(), 2*w, 2*h, theta, '#3d8ac0', "Kecimen")
+    # besni ellipses
+    xy2 = df[df["Class"] == "Besni"][[selected[0], selected[1]]]
+    cov2 = np.cov(xy2.transpose())
+    e_val, e_vec = np.linalg.eig(cov2)
+    w, h = 2 * np.sqrt(e_val)
+    theta = np.rad2deg(np.arccos(e_vec[0, 0]))
+    ell2 = gen_ellipse(xy2.mean(), w, h, theta, '#ffad64', "Besni")
+    ell21 = gen_ellipse(xy2.mean(), 2*w, 2*h, theta, '#ffad64', "Besni")
 
-    x2, y2 = list(df[df["Class"] == "Besni"][selected[0]]), list(df[df["Class"] == "Besni"][selected[1]])
-    pca.fit(list(zip(x2, y2)))
-    cov2 = np.cov(x2, y2)
-    lambda_, v = np.linalg.eig(cov2)
-    lambda_ = np.sqrt(lambda_)
-    ell2 = Ellipse(xy=(np.mean(x2), np.mean(y2)),
-                   width=lambda_[0] * 2, height=lambda_[1] * 2,
-                   angle=np.rad2deg(np.arccos(v[0, 0])),
-                   linewidth=2, ec='#ffad64', linestyle='', label="Besni")
-    ell2.set_alpha(0.2)
-    ell2.set_facecolor('#ffad64')
-    ell21 = Ellipse(xy=(np.mean(x2), np.mean(y2)),
-                    width=lambda_[0] * 4, height=lambda_[1] * 4,
-                    angle=np.rad2deg(np.arccos(v[0, 0])),
-                    linewidth=2, ec='#ffad64', linestyle='', label="Besni")
-    ell21.set_alpha(0.2)
-    ell21.set_facecolor('#ffad64')
-
+    # plot setup
     fig = plt.figure(facecolor="#0E1117")
     ax = fig.add_subplot(111)
     ax.set_facecolor('#262730')
-    plt.scatter(x, y, s=2)
-    plt.scatter(x2, y2, s=2)
+    plt.scatter(xy[selected[0]], xy[selected[1]], s=2)
+    plt.scatter(xy2[selected[0]], xy2[selected[1]], s=2)
     ax.set_xticks([])
     ax.set_yticks([])
     ax.tick_params(color='white', labelcolor='white')
@@ -101,8 +92,7 @@ else:
     ax.add_patch(ell2)
     ax.add_patch(ell1)
     ax.add_patch(ell21)
-    ax.set_xlim([min(min(x), min(x2)), max(max(x, x2))])
-    ax.set_ylim([min(min(y), min(y2)), max(max(y, y2))])
+
     ax.set_xlabel(selected[0], color="white")
     ax.set_ylabel(selected[1], color="white")
     ax.legend(["Kecimen", "Besni"])
